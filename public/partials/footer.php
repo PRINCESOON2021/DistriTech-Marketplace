ce2847c8bbdf5fd20f78583c44b023b9d622f092
PRINCESOON2021 <86659002+PRINCESOON2021@users.noreply.github.com>
2026-08-12 14:59:13 +0100

    Refresh corrected catalog script

--- public/partials/footer.php
+++ public/partials/footer.php
@@ -4,6 +4,6 @@
     <div><b>Solutions</b><a href="<?= e(url('index.php?category=cybersecurite#catalogue')) ?>">Cybersécurité</a><a href="<?= e(url('index.php?category=backup#catalogue')) ?>">Backup & PRA</a><a href="<?= e(url('index.php?category=sage#catalogue')) ?>">Sage</a></div>
     <div><b>Contact</b><a href="<?= e(url('quote.php')) ?>">Demander un devis</a><span>Maroc</span><small>Photo datacenter : Victor Grigas / Wikimedia Commons, CC BY-SA 3.0</small></div>
 </footer>
-<script src="<?= e(url('assets/js/app.js')) ?>"></script>
+<script src="<?= e(url('assets/js/app.js?v=20260812-2')) ?>"></script>
 </body>
 </html>
